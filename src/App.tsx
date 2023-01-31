8aa5c58228480cb91b7cfb871efd1353f9d3a558
guillaume-paris <guillaume1.paris@epitech.eu>
2023-01-31 11:13:05 +0100

    add the home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
 import React from 'react';
-import './App.css';
+import './styles/colors.css';
 import { useRoutes, Routes, Route } from 'react-router-dom';
 // Components
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
 import Resume from './components/Resume/Resume';
+import Contact from './components/Contact/Contact';
 import Header from './components/Header/Header';
+// Material UI
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Box from '@mui/material/Box';
 
-
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Poppins, sans-serif',
+    fontSize: 24,
+    h1: {
+      fontSize: 32,
+      color: '#fff',
+    },
+    h2: {
+      fontSize: 28,
+      color: '#fff',
+    },
+    h3: {
+      fontSize: 24,
+      color: '#fff',
+    },
+    h4: {
+      fontSize: 22,
+      color: '#fff',
+    },
+    h5: {
+      fontSize: 20,
+      color: '#fff',
+    },
+    h6: {
+      fontSize: 18,
+      color: '#fff',
+    },
+    body1: {
+      fontSize: 16,
+      color: '#fff',
+    },
+    body2: {
+      fontSize: 14,
+      color: '#fff',
+    },
+  },
+});
 
 const App: React.FC = () => {
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path ="/" element={<Home />} />
-        <Route path ="/about" element={<About />} />
-        <Route path ="/projects" element={<Projects />} />
-        <Route path ="/resume" element={<Resume />} />
-        <Route path='*' element={<h1>404 Page</h1>} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <Box className='bg-color'>
+          <Header />
+          <Routes>
+            <Route path ="/" element={<Home />} />
+            <Route path ="/about" element={<About />} />
+            <Route path ="/projects" element={<Projects />} />
+            <Route path ="/resume" element={<Resume />} />
+            <Route path ="/contact" element={<Contact />} />
+            <Route path='*' element={<h1>404 Page</h1>} />
+          </Routes>
+        </Box>
+      </ThemeProvider>
   </>
   );
 };
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+// Material UI
+import { Box } from '@mui/system';
+import { Typography, Button } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+// styles
+import './../../styles/colors.css';
+import './Home.css';
 
 const Home: React.FC = () => {
-  return <h1>Home Page</h1>;
+
+  const handleChipClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    console.log(event);
+  };
+
+  return (
+    <>
+      <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', gap: 3 }}>
+        <img src='/assets/globe-homepage.png' alt='globe-homepage' />
+        <Box className='home-page-text-part' sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+          <Typography variant='h3' className='frond-end-dev'>Front-end developer</Typography>
+          <Typography variant='h1' className='name-of-dev'>Guillaume Paris</Typography>
+          <Typography variant='h5'>Hi there ! I'm a Epitech student at the Nancy campus. I'm currently in my 3rd year of studies. In my professional experiences, I have worked with frameworks such as React or Angular. That's why I use them to make my own portfolio!</Typography>
+          <Box sx={{ display: 'flex', flexDirection: 'row', gap: 5 }}>
+            <Button variant='outlined' className='button-nav' component={Link} to='/about'><Typography variant='h5' className='button-nav-text'>About</Typography></Button>
+            <Button variant='outlined' className='button-nav' component={Link} to='/projects'><Typography variant='h5' className='button-nav-text'>Projects</Typography></Button>
+            <Button variant='outlined' className='button-nav' component={Link} to='/resume'><Typography variant='h5' className='button-nav-text'>Resume</Typography></Button>
+            <Button variant='outlined' className='button-nav' component={Link} to='/contact'><Typography variant='h5' className='button-nav-text'>Contact</Typography></Button>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 export default Home;
