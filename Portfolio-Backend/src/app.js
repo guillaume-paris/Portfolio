58986b1b5ecb441bd4ae73f97bf1b6238935806c
guillaume-paris <guillaume1.paris@epitech.eu>
2023-06-29 21:15:55 +0200

    [ADD] cors

--- Portfolio-Backend/src/app.js
+++ Portfolio-Backend/src/app.js
@@ -2,22 +2,23 @@ require('dotenv').config();
 require('./config/database.js');
 
 const express = require('express');
-const app = express();
 const swaggerUi = require('swagger-ui-express');
+const cors = require('cors');
 
 const swaggerDocument = require('./config/swagger');
 const authRoutes = require('./api/routes/authRoutes');
 
+const app = express();
+
+// Swagger setup
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-
+// Initialisation
 app.use(express.json());
+app.use(cors());
 
+// Main routes
 app.use('/api/auth', authRoutes);
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
@@ -27,4 +28,11 @@ app.use((err, req, res, next) => {
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`API server listening on port ${port}`);
+}).on('error', (err) => {
+  if (err.code === 'EADDRINUSE') {
+    console.log(`Port ${port} is already in use. Please free up the port and try again.`);
+    process.exit(1);
+  } else {
+    throw err;
+  }
 });
